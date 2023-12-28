977b5fc865cccdea32cae5a164a881c9e0b33a1f
roqisb <roqisb9@gmail.com>
2023-12-28 20:09:08 +0700

    add function InsertUiEvent

--- app/src/main/java/com/example/kontakaplikasiapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/kontakaplikasiapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.kontakaplikasiapi.ui.kontak.viewmodel
 
+import com.example.kontakaplikasiapi.model.Kontak
+
 class InsertViewModel {
 }
 data class InsertUiState(
@@ -11,4 +13,10 @@ data class InsertUiEvent(
     val nama: String = "",
     val email: String = "",
     val nohp: String = ""
+)
+fun InsertUiEvent.toKontak(): Kontak = Kontak(
+    id = id,
+    nama = nama,
+    alamat = email,
+    telpon = nohp
 )
