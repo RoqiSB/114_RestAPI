c4481057ff171a66722ef5597353e6b79a0d7873
roqisb <roqisb9@gmail.com>
2023-12-28 20:33:00 +0700

    add InsertViewModel to PenyediaViewModel

--- app/src/main/java/com/example/kontakaplikasiapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kontakaplikasiapi/ui/PenyediaViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.kontakaplikasiapi.KontakAplication
 import com.example.kontakaplikasiapi.ui.home.viewmodel.HomeViewModel
+import com.example.kontakaplikasiapi.ui.kontak.viewmodel.InsertViewModel
 
 object PenyediaViewModel{
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
@@ -15,6 +16,7 @@ object PenyediaViewModel{
         initializer {
             HomeViewModel(aplikasiKontak().container.kontakRepository)
         }
+        initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
     }
 }
 
