a4f7e20ae6589423329c4c6936a6335d6d2691fa
roqisb <roqisb9@gmail.com>
2023-12-28 20:35:50 +0700

    add function FormInputSiswa in InsertScreen

---
+++ app/src/main/java/com/example/kontakaplikasiapi/ui/kontak/screen/InsertScreen.kt
@@ -0,0 +1,23 @@
+package com.example.kontakaplikasiapi.ui.kontak.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.kontakaplikasiapi.ui.kontak.viewmodel.InsertUiEvent
+
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+
+    }
+}
